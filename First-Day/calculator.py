f409725568151ccdeda6c570d82367af0f47b9f2
swatisaulanki <100274438+swatisaulanki@users.noreply.github.com>
2023-08-24 16:23:40 +0530

    first commit

---
+++ First-Day/calculator.py
@@ -0,0 +1,6 @@
+a=50
+b=3
+print("the value of",a,"+",3, "is:", a+b)
+print("the value of",a,"-",3, "is:", a-b)
+print("the value of",a,"*",3, "is:", a*b)
+print("the value of",a,"%",3, "is:", a%b)
---
+++ First-Day/test.py
@@ -0,0 +1,22 @@
+print("hello \"swati\"\nhe")
+
+a=10
+print(a)
+b=True
+c="swati"
+d=None
+a1=9
+print(a+a1)
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
+
+list1=[8,2.3,[-4,5],["apple", "banana"]]
+print(list1)
+
+tuple1=(("parrot", "sparrow"),("lion","Tiger"))
+print(tuple)
+
+dict1={"name":"Swati", "age":20, "canVote":True}
+print(dict1)
